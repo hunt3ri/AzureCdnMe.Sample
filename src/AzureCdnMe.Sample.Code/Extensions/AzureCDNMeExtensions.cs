dfdbdab241edce051a765f0041c14b944d56f9f9
Iain Hunter <hunter_iain@yahoo.co.uk>
2012-06-27 16:02:25 +0100

    Initial working version of CDN extension

--- src/AzureCdnMe.Sample.Code/Extensions/AzureCDNMeExtensions.cs
+++ src/AzureCdnMe.Sample.Code/Extensions/AzureCDNMeExtensions.cs
@@ -5,52 +5,87 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Web.Mvc;
+using System.Collections;
 
 namespace AzureCdnMe.Sample.Code.Extensions
 {
 	public static class AzureCDNMeExtensions
 	{
+        // Expose as public property to ease unit testing without going to trouble of DI
+        public static string CdnAddress { get; set; }
+
+	    public static int RandomNum { get; set; }
+
+        static AzureCDNMeExtensions()
+        {
+            CdnAddress = ConfigurationManager.AppSettings["AzureCDNEndpoint"];
+        }
+
+
 		/// <summary>
 		/// Creates a URL pointing to the appropriate Azure CDN, includes a version number on query string to invalidate cache.
 		/// </summary>
 		/// <param name="helper">The helper.</param>
-		/// <param name="resource">The resource.</param>
+		/// <param name="contentLocation">The resource.</param>
 		/// <returns></returns>
-		public static string AzureCdnContent(this UrlHelper helper, string resource)
+		public static string AzureCdnContent(this UrlHelper helper, string contentLocation)
 		{
-			string versionNum = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            var queryString = "?version=" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		    var newLocation = FormatContentLocation(contentLocation, queryString);
 
-			var resourceUrl = string.Format("{0}/Content/{1}?version={2}",
-											ConfigurationManager.AppSettings["AzureCDNEndpoint"],
-											resource,
-											versionNum);
-
-			return helper.Content(resourceUrl);
+			return helper.Content(newLocation);
 		}
 
-		public static string AzureCdnContent(this UrlHelper helper, string resource, bool randomQueryString)
+        /// <summary>
+        /// Creates a URL pointing to the appropriate Azure CDN, includes a random number on querystring to invalidate cache
+        /// This may reset randomly (ironically) if server resets etc, so you should ideally use the version number approach
+        /// if you can
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="contentLocation"></param>
+        /// <param name="randomQueryString"></param>
+        /// <returns></returns>
+        public static string AzureCdnContent(this UrlHelper helper, string contentLocation, bool randomQueryString)
 		{
-			var random = new Random().Next().ToString();
+            // Only get a new number the first time the app loads.
+            if (RandomNum == 0)
+                RandomNum = new Random().Next();
 
-			var resourceUrl = string.Format("{0}/Content/{1}?version={2}",
-											ConfigurationManager.AppSettings["AzureCDNEndpoint"],
-											resource,
-											random);
+            var queryString = "?version=" + RandomNum.ToString();
+            var newLocation = FormatContentLocation(contentLocation, queryString);
 
-			return helper.Content(resourceUrl);
+            return helper.Content(newLocation);
 		}
 
-		private static string FormatResource(string resource)
+        public static string FormatContentLocation(string contentLocation, string queryString)
 		{
 			var token = new char[] {'/'};
 
-//			var resourceArray = 
-//
-//			var cdnAddress = ConfigurationManager.AppSettings["AzureCDNEndpoint"].ToLower();
-//
-//			if (cdnAddress.StartsWith("http"))
+            var locationArray = contentLocation.Split(token);
+            var locationArrayList = new ArrayList(locationArray);
 
-			throw new NotImplementedException();
+            // Create CDN Address as array so we can insert it into url array at appropriate point
+            var cdnAddressArray = new List<string> { CdnAddress };
+
+            // Replace root element with http address for CDN
+            if (CdnAddress.StartsWith("http") && (locationArrayList[0].ToString() == "~"))
+            {
+                locationArrayList[0] = CdnAddress;
+            }
+            else if (locationArrayList[0].ToString() == "~")
+            {
+
+                locationArrayList.InsertRange(1, cdnAddressArray);
+            }
+            else
+            {
+                locationArrayList.InsertRange(0, cdnAddressArray);
+            }
+
+            var newUrl = String.Join("/", locationArrayList.ToArray());
+            newUrl = newUrl + queryString;
+
+            return newUrl;
 		}
 
 
---
+++ src/AzureCdnMe.Sample.Tests/AzureCdnMeExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Web.Mvc;
+using Moq;
+using System.Web;
+using System.Web.Routing;
+using AzureCdnMe.Sample.Code.Extensions;
+
+namespace AzureCdnMe.Sample.Tests
+{
+    [TestFixture]
+    public class AzureCdnMeExtensionsTests
+    {
+        [Test]
+        public void If_CDN_EndPoint_Does_Not_Start_Http_Then_Insert_Folder_Appropriately()
+        {
+            // Arrange
+            AzureCDNMeExtensions.CdnAddress = "CDN";
+
+            // Act
+            var cdnUrl = AzureCDNMeExtensions.FormatContentLocation("~/Content/bootstrap.css", "?version=1.0.0.0");
+
+            //Assert
+            Assert.That(cdnUrl, Is.EqualTo("~/CDN/Content/bootstrap.css?version=1.0.0.0"));
+
+           
+        }
+    }
+}
